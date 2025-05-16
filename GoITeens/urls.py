9ca779eb684dc8864851ee230b7639caf7d4c2d4
Gakyra <maks3010dan@gmail.com>
2025-05-16 17:39:24 +0300

    1

--- GoITeens/urls.py
+++ GoITeens/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('market.urls')),
     # path('example2/', include('example2.urls')),
-    path("user_permission/", include('user_permission.urls')),
-    path('example4/', include('example4.urls')),
-    path('sygma/', include('sygma.urls')),
-    path('mails/', include('mails.urls')),
-    path('ajax_demo/', include('ajax_demo.urls'))
+    # path("user_permission/", include('user_permission.urls')),
+    # path('example4/', include('example4.urls')),
+    # path('sygma/', include('sygma.urls')),
+    # path('mails/', include('mails.urls')),
+    # path('ajax_demo/', include('ajax_demo.urls'))
 
 
 ]
